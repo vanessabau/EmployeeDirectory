8cdd95f7beccb9e603ab81c92465caf0a81840d2
Vanessa Bautista <vbautista916@icloud.com>
2020-08-17 18:39:40 -0700

    empty

--- employeedirectory/src/components/utils/paginate.js
+++
@@ -1,8 +0,0 @@
-import _ from "lodash";
-
-export function Paginate(items, pageNumber, pageSize) {
-  const startIndex = (pageNumber - 1) * pageSize;
-  //convert items array to a lodash wrapper
-  return _(items).slice(startIndex).take(pageSize).value();
-  //use lodash to slice our array starting at the new index
-}
